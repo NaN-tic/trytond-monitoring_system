7fba3f5abfa6bfbac0aaf0d01594874c49e7fb05
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-10-15 16:31:13 +0200

    Remove view tests as module has no views

--- tests/test_network_monitoring_system.py
+++ tests/test_network_monitoring_system.py
@@ -3,7 +3,7 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends  # , doctest_dropdb TODO: Remove if no sceneario needed.
+from trytond.tests.test_tryton import test_depends
 
 
 class TestCase(unittest.TestCase):
@@ -11,10 +11,6 @@ class TestCase(unittest.TestCase):
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('network_monitoring_system')
-
-    def test0005views(self):
-        'Test views'
-        test_view('network_monitoring_system')
 
     def test0006depends(self):
         'Test depends'
@@ -24,8 +20,4 @@ class TestCase(unittest.TestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_network_monitoring_system.rst',
-    #        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
